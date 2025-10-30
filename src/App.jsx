8f2d2f95c390923a4d403c78abe4836109bd0c09
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-10-30 19:20:04 +0000

    add SASS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
+import './App.scss'
 import Header from './components/header';
 import Home from './pages/Home';
 import About from './pages/About';

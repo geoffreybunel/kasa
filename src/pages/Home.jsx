ed7e993d947c609e4f7c3916048e8f500f83e7ac
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-11-06 00:26:21 +0100

    responsive hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
+import './Home.scss'
 import Hero from "../components/Hero"
 import Cards from "../components/Cards"
 
 function Home() {
     return (
-        <div>
+        <div className='Home__container'>
             <Hero className = "Hero__Home" text="Chez vous, partout et ailleurs"/>
             <Cards />
         </div>

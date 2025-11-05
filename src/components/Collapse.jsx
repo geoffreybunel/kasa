2500b193bc2e8db80d7ff899e3aee6b0cf7a85e1
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-11-05 17:31:13 +0100

    collapse animation

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,7 +1,29 @@
-function Collapse() {
+import './Collapse.scss'
+import { useState } from 'react';
+
+function Collapse({title, description}) {
+    const [dropdownToggled, setdropdownToggled] = useState(false);
+
+    // function openItem() {
+    //     setDropdown(true);
+    // }
+
+    // function closeItem() {
+    //     setDropdown(false);
+    // }
     return (
         <div>
-            
+            <div className='dropdown'>
+                <h3>{title}</h3>
+                <img 
+                    src="src/assets/arrow.png" 
+                    alt="arrow" 
+                    className={dropdownToggled ? 'rotated' : ''}
+                    onClick={() => {setdropdownToggled(!dropdownToggled)}} />
+            </div>
+            <div className={`options ${dropdownToggled ? 'visible' : ''}`}>
+                <p>{description}</p>
+            </div>
         </div>
     )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,14 @@
+import Collapse from "../components/Collapse"
 import Hero from "../components/Hero"
+import data from '../data/collapse.json'
 
 function About() {
     return (
         <div>
             <Hero className = "Hero__About" text=""/>
+            {data.map(item => (
+                <Collapse key={item.title} title={item.title} description={item.description} />
+            ))}
         </div>
     )
 }
